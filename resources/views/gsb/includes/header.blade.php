faee308d27d2ddd714092967c4a7604f1455075c
iamalexly <alexlebaillypro@gmail.com>
2018-10-11 11:32:22 +0200

    Ajout du logo dans la navbar qui redirige vers l'index

--- resources/views/gsb/includes/header.blade.php
+++ resources/views/gsb/includes/header.blade.php
@@ -1,3 +1,3 @@
 <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">LSW42 - GSB - Laravel</a>
+    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="{{ route("gsb.fiches.index") }}"><img src="{{asset('logo.png')}}" width="100" height="50"></a>
 </nav>
